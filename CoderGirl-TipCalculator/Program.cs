8cf22e4a9b6f04891c90447b910027fc99064c29
msunkist99 <msunkist99@gmail.com>
2019-02-13 18:12:38 -0600

    Add project files.

---
+++ CoderGirl-TipCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoderGirl_TipCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
